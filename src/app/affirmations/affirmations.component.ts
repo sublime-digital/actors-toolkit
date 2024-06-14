3693230d02d5b3689c3d777e205cc73e3bdf691c
sds <sublimedigitalsolutions@gmail.com>
2024-06-13 21:18:59 -0400

    affirmations module fixed up and favicon added
    there is now a button as well as a favicon

--- src/app/affirmations/affirmations.component.ts
+++ src/app/affirmations/affirmations.component.ts
@@ -12,4 +12,22 @@ export class AffirmationsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  affirmation = ""
+
+  allaffirmation = [
+    "today will be an awesome day",
+    "i am hopeful for all my tomorrows",
+    "i am letting go of the painful memories",
+    "i am leading my life from a place of love",
+    "i deserve the good things only if i work hard for them"
+  ]
+
+  getNew() {
+    this.affirmation = this.allaffirmation[Math.floor(Math.random()*this.allaffirmation.length)];
+  }
+
+submitted = false;
+
+onSubmit() { this.submitted = true; }
 }
+
