be4d6e368da5c2ba8e93100ef2af697453bdc2f5
sds <sublimedigitalsolutions@gmail.com>
2026-08-23 23:52:46 +0600

    v17, adverts added
    versioning to v17. adverts added to all pages. Imdb service added

--- src/app/mood-record/mood-record.component.ts
+++ src/app/mood-record/mood-record.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, signal, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mood-record',
@@ -7,16 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoodRecordComponent implements OnInit {
 
+
+    private images = [
+        '../../assets/adverts/advert001.png',
+        '../../assets/adverts/advert002.png',
+        '../../assets/adverts/advert003.png',
+        '../../assets/adverts/advert004.png',
+      ];
+
+    private links = [
+        "https://www.youtube.com/@AllDay-Foodie",
+        "/",
+        "https://www.patreon.com/plants_and_poetry/shop/zenfit-spinning-classes-10-videos-1750922",
+        "/",
+        "/",
+      ];
+
+          // 2. State management with signals
+      private currentIndex = signal(0);
+      private timerId: any;
+
+      // 3. Computed signal for the template
+      readonly currentImage = computed(() => this.images[this.currentIndex()]);
+      readonly currentLink = computed(() => this.links[this.currentIndex()]);
+
+      ngOnInit() {
+        this.startSwitching();
+      }
+
+      ngOnDestroy(): void {
+              if (this.timerId) clearInterval(this.timerId);
+      }
+
+      startSwitching() {
+        this.timerId = setInterval(() => {
+          this.currentIndex.update(idx => (idx + 1) % this.images.length);
+        }, 5000); // 5000ms = 5 seconds
+      }
+
+
   constructor() { }
-
-
-  ngOnInit(): void {
-  }
 
   goodmood = false;
 
   logMood(): void {
-    
+
   }
 
   sound = new Audio("../../assets/bell.wav");
--- src/app/traits/traits.component.ts
+++ src/app/traits/traits.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, signal, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-traits',
@@ -7,13 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TraitsComponent implements OnInit {
 
+
+  private images = [
+      '../../assets/adverts/advert001.png',
+      '../../assets/adverts/advert002.png',
+      '../../assets/adverts/advert003.png',
+      '../../assets/adverts/advert004.png',
+    ];
+
+  private links = [
+      "https://www.youtube.com/@AllDay-Foodie",
+      "/",
+      "https://www.patreon.com/plants_and_poetry/shop/zenfit-spinning-classes-10-videos-1750922",
+      "/",
+      "/",
+    ];
+
+        // 2. State management with signals
+    private currentIndex = signal(0);
+    private timerId: any;
+
+    // 3. Computed signal for the template
+    readonly currentImage = computed(() => this.images[this.currentIndex()]);
+    readonly currentLink = computed(() => this.links[this.currentIndex()]);
+
+    ngOnInit() {
+      this.startSwitching();
+    }
+
+    ngOnDestroy(): void {
+            if (this.timerId) clearInterval(this.timerId);
+    }
+
+    startSwitching() {
+      this.timerId = setInterval(() => {
+        this.currentIndex.update(idx => (idx + 1) % this.images.length);
+      }, 5000); // 5000ms = 5 seconds
+    }
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   goodmoods = false;
-  
+
   sound = new Audio("../../assets/bell.wav");
   playSound() {
     this.sound.play();
