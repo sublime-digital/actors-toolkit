7b8307134f51678e8f2d107db137e7de2c3c4e80
sds <sublimedigitalsolutions@gmail.com>
2026-09-03 21:04:43 +0600

    env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  omdbApiKey: process.env['NG_APP_OMDB_API_KEY'] || 'PLACEHOLDER_KEY',
+  omdbApiKey: process.env['NG_APP_OMDB_API_KEY'] || '74efadf3',
 };
